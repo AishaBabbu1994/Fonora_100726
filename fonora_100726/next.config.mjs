715ff16ddb3aa8ecd4a208fb133f6974a80fe7d9
Aisha Babbü <aishababbu94@gmail.com>
2026-07-10 16:26:20 -0400

    Add files via upload

---
+++ fonora_100726/next.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [],
+  },
+  env: {
+    GROQ_API_KEY: process.env.GROQ_API_KEY,
+  },
+}
+module.exports = nextConfig
